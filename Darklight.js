f2db20cc6e1ea02b8dddcc4173a19aac6c6136f9
MadCoffee <aus05btitvas@gmail.com>
2024-11-08 14:27:54 +0100

    Working button?

--- Darklight.js
+++ Darklight.js
@@ -1,24 +1,23 @@
-let darkmode = localStorage.getItem('darkmode')
-const themeSwitch = document.getElementById('theme-switch')
+let darkmode = localStorage.getItem('darkmode');
+const themeSwitch = document.getElementById('theme-switch');
 
 const enableDarkmode = () => {
-    document.body.classList.add('darkmode')
-    document.body.classList.remove('lightmode')
-    localStorage.setItem('darkmode', 'active')
+    document.body.classList.add('darkmode');
+    document.body.classList.remove('lightmode');
+    localStorage.setItem('darkmode', 'active');
 }
-
 function disableDarkmode() {
-    document.body.classList.add('lightmode')
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
+    document.body.classList.add('lightmode');
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
 }
 
-if (darkmode === "active") enableDarkmode()
-else disableDarkmode()
+if (darkmode === "active") enableDarkmode();
+else disableDarkmode();
 
 themeSwitch.addEventListener('click', () => {
-    darkmode = localStorage.getItem('darkmode')
-    console.log(darkmode)
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+    darkmode = localStorage.getItem('darkmode');
+    console.log(darkmode);
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
     
-})
+});
